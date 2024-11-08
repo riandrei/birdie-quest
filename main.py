af135d3cd48c09efece8ac2799e98ac77d92ef8a
Riandrei Casañas <riandrei.casanas@gmail.com>
2024-11-08 21:18:54 +0800

    finished the background

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import configs
+import assets
+from objects.background import Background
 
 
 pygame.init()
@@ -8,12 +10,22 @@ screen = pygame.display.set_mode((configs.SCREEN_WIDTH, configs.SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
+assets.load_sprites()
+
+sprites = pygame.sprite.LayeredUpdates()
+
+Background(0, sprites)
+Background(1, sprites)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
     screen.fill("black")
+
+    sprites.draw(screen)
+    sprites.update()
 
     pygame.display.flip()
     clock.tick(configs.FPS)
